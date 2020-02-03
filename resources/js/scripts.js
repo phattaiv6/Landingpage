ace06c78f0d55d409dc3c013e6aa2a7fcc089a1d
phattaiv6 <luonghuynhphattai@gmail.com>
2020-02-03 15:16:24 +0700

    first commit

---
+++ resources/js/scripts.js
@@ -0,0 +1,17 @@
+/*$(document).ready(
+
+    function(){
+        $('.about section').waypoint(
+            function(direction){
+if(direction == "down"){
+    $('nav').addClass ('sticky')
+}else{
+    $('nav').removeClass('sticky');
+}
+            }, {
+                offset:'600px'
+            }
+
+        )
+    }
+) */
